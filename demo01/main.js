00290a4e6031df5e4cc4ea26ee5b07a1d61083e4
Fortune.Zhang <zhangfortune527@gmail.com>
2013-07-13 10:04:30 +0800

    first spec

---
+++ demo01/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: fortunezhang
+ * Date: 13-7-13
+ * Time: 上午9:57
+ * To change this template use File | Settings | File Templates.
+ */
+
+function InvoiceCntl($scope) {
+    $scope.count = 0 ;
+
+}
